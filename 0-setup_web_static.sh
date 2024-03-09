104835119edf1ad1984402eef8102a3549fdbff0
mohamed-bo <medprgrm@gmail.com>
2024-03-09 21:32:37 +0100

    inchalah it works

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,59 @@
 #!/usr/bin/env bash
-# web srt.
+# install config edit index strat ngix
+SERVER_NGIX="server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
 
-apt-get update
-apt-get install -y nginx
+	server_name _;
+	index index.html index.htm;
+	error_page 404 /404.html;
+	add_header X-Served-By \$hostname;
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+	location / {
+		root /var/www/html/;
+		try_files \$uri \$uri/ =404;
+	}
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+		try_files \$uri \$uri/ =404;
+	}
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $hostname;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://intranet.alxswe.com/projects/288;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+	if (\$request_filename ~ redirect_me) {
+		rewrite ^ https://intranet.alxswe.com/projects/288 permanent;
+	}
 
-service nginx restart
+	location = /404.html {
+		root /var/www/error/;
+		internal;
+	}
+}"
+INDEX_PAGE="
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+"
+if [[ "$(which nginx | grep -c nginx)" == '0' ]]; then
+    apt-get update
+    apt-get -y install nginx
+fi
+sudo mkdir -p /var/www/html /var/www/error
+sudo chmod -R 755 /var/www
+sudo echo 'Hello World!' > /var/www/html/index.html
+sudo echo -e "Ceci n\x27est pas une page" > /var/www/error/404.html
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo echo -e "$INDEX_PAGE" > /data/web_static/releases/test/index.html
+[ -d /data/web_static/current ] && sudo rm -rf /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data
+sudo bash -c "echo -e '$SERVER_NGIX' > /etc/nginx/sites-available/default"
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+if [ "$(pgrep -c nginx)" -le 0 ]; then
+	sudo service nginx start
+else
+	sudo service nginx restart
+fi
