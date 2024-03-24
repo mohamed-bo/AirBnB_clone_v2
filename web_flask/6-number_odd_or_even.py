0c35bea4ae3c35335969db348171906ebb38d6e2
mohamed-bo <medprgrm@gmail.com>
2024-03-24 15:49:03 +0100

    inchalah it works

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""start Flash Web App"""
+from flask import Flask, render_template
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    '''index'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''hbnp path'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def cPath(text):
+    '''c path'''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonPath(text='is cool'):
+    '''python Path'''
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def numberPath(n):
+    '''numberPath'''
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def numberTemplatePath(n):
+    '''numberTemplatePath'''
+    dictio = {
+        'n': n
+    }
+    return render_template('5-number.html', **dictio)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    '''numberoddor even'''
+    diction = {
+        'n': n
+    }
+    return render_template('6-number_odd_or_even.html', **diction)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
